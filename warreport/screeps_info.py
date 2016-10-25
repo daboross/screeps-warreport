daboross/screeps-warreport

commit 5ae34f0e104d9dabec930b586ab3e5c4faaa1676

Move to completely directly using requests to access the Screeps API.
Eventually we should move back to using python-screeps for all API calls, but at the moment it's much easier to handle and log errors specifically by calling the API directly. This way, all errors are correctly logged, and the behavior is as expected. (rather than being printed to console as python-screeps does.)

warreport/screeps_info.py
@@ -8,8 +8,11 @@ from warreport.constants import scout, civilian, general_attacker, dismantling_a
     ranged_attacker
 from . import data_caching
 
-USERNAME_URL_FORMAT = "https://screeps.com/api/user/find?id={user_id}"
-HISTORY_URL_FORMAT = "https://screeps.com/room-history/{room}/{tick}.json"
+_URL_ROOT = "https://screeps.com/"
+
+USERNAME_URL_FORMAT = _URL_ROOT + "api/user/find"
+HISTORY_URL_FORMAT = _URL_ROOT + "room-history/{room}/{tick}.json"
+BATTLES_URL_FORMAT = _URL_ROOT + "api/experimental/pvp"
 
 logger = logging.getLogger("warreport")
 
@@ -22,11 +25,31 @@ class ScreepsError(Exception):
         return self.message
 
 
+def grab_battles_uncached(since_tick=None, interval=None):
+    if since_tick is not None:
+        params = {'sinceTick': since_tick}
+    elif interval is not None:
+        params = {'interval': interval}
+    else:
+        return None
+    result = requests.get(BATTLES_URL_FORMAT, params=params)
+    if not result.ok:
+        raise ScreepsError("{} ({}, at {})".format(result.text, result.status_code, result.url))
+
+    json = result.json()
+    if not json:
+        raise ScreepsError("Invalid json: {} ({}, at {})".format(result.text, result.status_code, result.url))
+    if not json.get('ok'):
+        raise ScreepsError("Result without 'ok' property: {} ({}, at {})".format(result.text, result.status_code,
+                                                                                 result.url))
+    return json
+
+
 def username_from_id(user_id):
     cached = data_caching.get_username(user_id)
     if cached is not None:
         return cached
-    call_result = requests.get(USERNAME_URL_FORMAT.format(user_id=user_id))
+    call_result = requests.get(USERNAME_URL_FORMAT, params={'user_id': user_id})
     if call_result.ok:
         name = call_result.json().get('user', {}).get('username', None)
         if name is None:

warreport/__init__.py
@@ -3,9 +3,7 @@ import logging.config
 
 import os
 import redis
-from screepsapi import screepsapi
 
-screeps_api = screepsapi.API()
 SLACK_URL = None
 redis_conn = None
 

warreport/battle_monitor.py
@@ -3,7 +3,8 @@ import logging
 
 from functools import partial
 
-from warreport import redis_conn as redis, screeps_api, data_caching, screeps_info
+from warreport import redis_conn as redis, data_caching, screeps_info
+from warreport.screeps_info import ScreepsError
 
 _LAST_CHECKED_TICK_KEY = "screeps:warreport:last-checked-tick"
 _LAST_CHECKED_EXPIRE_SECONDS = 30 * 60
@@ -19,11 +20,16 @@ def grab_new_battles(loop):
     """
     last_grabbed_tick = yield from loop.run_in_executor(None, redis.get, _LAST_CHECKED_TICK_KEY)
     while True:
-        if last_grabbed_tick is not None:
-            battles = yield from loop.run_in_executor(None, partial(screeps_api.battles, sinceTick=last_grabbed_tick))
+        try:
+            if last_grabbed_tick is not None:
+                battles = yield from loop.run_in_executor(None, partial(screeps_info.grab_battles_uncached,
+                                                                        since_tick=last_grabbed_tick))
+            else:
+                battles = yield from loop.run_in_executor(None, partial(screeps_info.grab_battles_uncached,
+                                                                        interval=2000))
+        except ScreepsError as e:
+            logging.warning("Error accessing battles API: {}".format(e))
         else:
-            battles = yield from loop.run_in_executor(None, partial(screeps_api.battles, interval=2000))
-        if battles and battles.get('ok'):
             last_grabbed_tick = battles.get('time')
             if len(battles['rooms']):
                 yield from asyncio.gather(

commit d907b1b3383b965a5ac06803f5a3ca1ad9f1db89

Fix bug introduced by 5ae34f0e104d9dabec930b586ab3e5c4faaa1676 - an incorrect call to the username finding API.
This is one reason why it would be nice to use python-screeps. For now though, everything (and now that too) does seem to be functioning correctly.

warreport/screeps_info.py
@@ -49,7 +49,7 @@ def username_from_id(user_id):
     cached = data_caching.get_username(user_id)
     if cached is not None:
         return cached
-    call_result = requests.get(USERNAME_URL_FORMAT, params={'user_id': user_id})
+    call_result = requests.get(USERNAME_URL_FORMAT, params={'id': user_id})
     if call_result.ok:
         name = call_result.json().get('user', {}).get('username', None)
         if name is None: